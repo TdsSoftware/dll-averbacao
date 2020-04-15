24e323841b57a6d4d8a7afd5e87c659aa605a321
Matheus Ravanello Mokwa <matheus.mokwa@gmail.com>
2020-04-15 16:37:06 -0300

    Initial Commit

---
+++ TDS.Averbacao/Services/AverbacaoATeM.cs
@@ -0,0 +1,17 @@
+﻿using net.r_eg.DllExport;
+using TDS.Averbacao.br.com.averba.webserver;
+
+namespace TDS.Averbacao.Services
+{
+    public class AverbacaoATeM
+    {
+        public readonly static ATMWebSvr client = new ATMWebSvr();
+
+        [DllExport]
+        public static string AverbarCTe()
+        {
+            var result = client.averbaCTe("usuario", "senha", "codatm", "xmlCte");
+            return result.ToString();
+        }
+    }
+}
